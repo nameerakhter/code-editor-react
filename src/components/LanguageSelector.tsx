b391bb7927ee50581ba78b0b7803ddb2f3a863ba
nameerakhter <akhtarnameer@gmail.com>
2024-11-23 19:17:50 +0530

    add: Language selector dropdown menu

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,3 +1,36 @@
-export default function LanguageSelector() {
-  return <div>this is a langage slector dropdown menu</div>;
+import { LanguagesSupported } from "../../types/index.ts";
+import { Languages } from "../../types/languages.ts";
+
+interface LanguageSelectorProps {
+  selectedLanguage: LanguagesSupported;
+  onLanguageChange: (language: LanguagesSupported) => void;
+}
+export default function LanguageSelector({
+  selectedLanguage,
+  onLanguageChange,
+}: LanguageSelectorProps) {
+  return (
+    <div className="border-1 border-black  ">
+      <select
+        value={selectedLanguage.value}
+        onChange={(e) => {
+          const selected = Languages.find(
+            (lang) => lang.value === e.target.value,
+          );
+          if (selected) {
+            onLanguageChange(selected);
+          }
+        }}
+        className="px-3 py-2 border rounded-md mb-4 font-mono text-sm"
+      >
+        {Languages.map((lang) => {
+          return (
+            <option key={lang.label} value={lang.value}>
+              {lang.label}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
 }
--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 import Prism from "prismjs";
-import { TextAreaprops } from "../../types/index.ts";
+import { LanguagesSupported, TextAreaprops } from "../../types/index.ts";
+import LanguageSelector from "./LanguageSelector.tsx";
+import { Languages } from "../../types/languages.ts";
 
 export default function TextArea({
   initialCode = "",
-  language,
+  language = Languages[0],
 }: TextAreaprops) {
   const [code, setCode] = useState<string>(initialCode);
+  const [selectedLanguage, setSelectedLanguage] =
+    useState<LanguagesSupported>(Languages);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -20,30 +24,41 @@ export default function TextArea({
     textAreaHeight.style.height = "auto";
     textAreaHeight.style.height = `${Math.min(textAreaHeight.scrollHeight, 400)}px`;
   }
+  function handleLanguageChange(newLanguage: LanguagesSupported) {
+    setSelectedLanguage(newLanguage);
+  }
   return (
-    <div className={`relative mx-auto `}>
-      {/* FIX:  the text area is scrollable but the highlight is not scrollable*/}
-      <pre
-        className=" h-full absolute inset-0 m-0 pt-0 px-[1em] overflow-hidden whitespace-pre-wrap font-mono leading-normal"
-        style={{
-          color: "rgba(255, 255, 255, 0.8)",
-          pointerEvents: "none",
-        }}
-      >
-        <code className={`language-${language.value}`}>{code}</code>
-      </pre>
+    <>
+      <div>
+        <LanguageSelector
+          selectedLanguage={selectedLanguage}
+          onLanguageChange={handleLanguageChange}
+        />
+      </div>
+      <div className={`relative mx-auto `}>
+        {/* FIX:  the text area is scrollable but the highlight is not scrollable*/}
+        <pre
+          className=" h-full absolute inset-0 m-0 pt-0 px-[1em] overflow-hidden whitespace-pre-wrap font-mono leading-normal"
+          style={{
+            color: "rgba(255, 255, 255, 0.8)",
+            pointerEvents: "none",
+          }}
+        >
+          <code className={`language-${language.value}`}>{code}</code>
+        </pre>
 
-      {/* TODO: move it to styles.css and make a seperate class  */}
-      <textarea
-        value={code}
-        onChange={handleChange}
-        className="relative w-full py-[1.5rem] px-[1rem] bg-transparent font-mono leading-normal resize-none"
-        style={{
-          caretColor: "white",
-          color: "transparent",
-          outline: "none",
-        }}
-      />
-    </div>
+        {/* TODO: move it to styles.css and make a seperate class  */}
+        <textarea
+          value={code}
+          onChange={handleChange}
+          className="relative w-full py-[1.5rem] px-[1rem] bg-transparent font-mono leading-normal resize-none"
+          style={{
+            caretColor: "white",
+            color: "transparent",
+            outline: "none",
+          }}
+        />
+      </div>
+    </>
   );
 }
